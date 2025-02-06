3344d7a845ec3bd91c2e01b1485262b4e6b0352f
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2025-02-07 02:25:55 +0300

    Create v1.0.0-autoscript.sh

---
+++ v1.0.0-autoscript.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+LOG_FILE="/var/log/atomone_node_install.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+printGreen() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+printLine() {
+    echo "------------------------------"
+}
+
+# Function to print the node logo
+function printNodeLogo {
+    echo -e "\033[32m"
+    echo "          
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+█████████████████████████████████████                          █████████████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+██████████████████████████████             █             █            ██████████████████████████████
+████████████████████████████           █████             ████           ████████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ███████            ██████          ███████████████████████████
+████████████████████████████          ██████████         ██████          ███████████████████████████
+████████████████████████████          █████████████      ██████          ███████████████████████████
+████████████████████████████             █████████████     ████          ███████████████████████████
+████████████████████████████          █     █████████████     █          ███████████████████████████
+████████████████████████████          █████     ████████████             ███████████████████████████
+████████████████████████████          ██████       ████████████          ███████████████████████████
+████████████████████████████          ██████          █████████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████            ████             ███            ████████████████████████████
+██████████████████████████████                                        ██████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+█████████████████████████████████████                           ████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+Hazen Network Solutions 2025 All rights reserved."
+    echo -e "\033[0m"
+}
+
+# Show the node logo
+printNodeLogo
+
+# User confirmation to proceed
+echo -n "Type 'yes' to start the installation Atomone Testnet v1.0.0 and press Enter: "
+read user_input
+
+if [[ "$user_input" != "yes" ]]; then
+  echo "Installation cancelled."
+  exit 1
+fi
+
+# Function to print in green
+printGreen() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+printGreen "Starting installation..."
+sleep 1
+
+printGreen "If there are any, clean up the previous installation files"
+
+sudo systemctl stop atomoned
+sudo systemctl disable atomoned
+sudo rm -rf /etc/systemd/system/atomoned.service
+sudo rm $(which atomoned)
+sudo rm -rf $HOME/.atomone
+sed -i "/ATOMONE_/d" $HOME/.bash_profile
+
+# Update packages and install dependencies
+printGreen "1. Updating and installing dependencies..."
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# User inputs
+read -p "Enter your MONIKER: " MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (2-digit): " PORT
+echo 'export PORT='$PORT
+
+# Setting environment variables
+echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+echo "export ATOMONE_CHAIN_ID=\"atomone-testnet-1\"" >> $HOME/.bash_profile
+echo "export ATOMONE_PORT=$PORT" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Chain ID:       \e[1m\e[32m$ATOMONE_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$ATOMONE_PORT\e[0m"
+printLine
+sleep 1
+
+# Install Go
+printGreen "2. Installing Go..." && sleep 1
+cd $HOME
+VER="1.23.1"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Version check
+echo $(go version) && sleep 1
+
+# Download Prysm protocol binary
+printGreen "3. Downloading Atomone binary and setting up..." && sleep 1
+cd $HOME
+rm -rf atomone
+git clone https://github.com/atomone-hub/atomone
+cd atomone
+git checkout v1.0.0
+make install
+
+mkdir -p ~/.atomone/cosmovisor/genesis/bin
+mv $HOME/atomone/build/atomoned ~/.atomone/cosmovisor/genesis/bin/
+
+sudo ln -s ~/.atomone/cosmovisor/genesis ~/.atomone/cosmovisor/current -f
+sudo ln -s ~/.atomone/cosmovisor/current/bin/atomoned /usr/local/bin/atomoned -f
+
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.6.0
+
+# Create service file
+sudo tee /etc/systemd/system/atomoned.service > /dev/null << EOF
+[Unit]
+Description=atomone node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start --home $HOME/.atomone
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=${HOME}/.atomone"
+Environment="DAEMON_NAME=atomoned"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:~/.atomone/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable atomoned
+
+# Initialize the node
+printGreen "7. Initializing the node..."
+atomoned config node tcp://localhost:${ATOMONE_PORT}657
+atomoned config keyring-backend os
+atomoned config chain-id atomone-testnet-1
+atomoned init $MONIKER --chain-id atomone-testnet-1
+
+# Download genesis and addrbook files
+printGreen "8. Downloading genesis and addrbook..."
+wget -O $HOME/.atomone/config/genesis.json https://server-2.itrocket.net/testnet/atomone/genesis.json
+wget -O $HOME/.atomone/config/addrbook.json  https://raw.githubusercontent.com/hazennetworksolutions/atomone-testnet/refs/heads/main/addrbook.json
+
+
+# Configure gas prices and ports
+printGreen "9. Configuring custom ports and gas prices..." && sleep 1
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.001uatone"|g' $HOME/.atomone/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.atomone/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.atomone/config/config.toml
+
+sed -i.bak -e "s%:1317%:${ATOMONE_PORT}317%g;
+s%:8080%:${ATOMONE_PORT}080%g;
+s%:9090%:${ATOMONE_PORT}090%g;
+s%:9091%:${ATOMONE_PORT}091%g;
+s%:8545%:${ATOMONE_PORT}545%g;
+s%:8546%:${ATOMONE_PORT}546%g;
+s%:6065%:${ATOMONE_PORT}065%g" $HOME/.atomone/config/app.toml
+
+# Configure P2P and ports
+sed -i.bak -e "s%:26658%:${ATOMONE_PORT}658%g;
+s%:26657%:${ATOMONE_PORT}657%g;
+s%:6060%:${ATOMONE_PORT}060%g;
+s%:26656%:${ATOMONE_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${ATOMONE_PORT}656\"%;
+s%:26660%:${ATOMONE_PORT}660%g" $HOME/.atomone/config/config.toml
+
+# Set up seeds and peers
+printGreen "10. Setting up peers and seeds..." && sleep 1
+SEEDS="85e441cfe74b8c0f8b820beff46edab20e92716c@atomone-testnet-seed.itrocket.net:62657"
+PEERS="bddf062a8328bcc50e37f83448e770e2e385c72f@atomone-testnet-peer.itrocket.net:62656,4adfce6fcbd3dc61109d8c67801272a162ecd29e@116.202.210.177:62656,14dbb758d8805a146497227caafe224a4ea29c2b@46.232.248.39:19656,dd27a23e0adc98d6dc53802d95ce581b06723845@185.252.233.217:26656,29d901d882d40048124670f6ea902cd933c9aa36@37.60.255.34:26656,5fcea85e54d69b50d23c759ea5f057c0fea6243b@[2a03:cfc0:8000:13::b910:27be]:11756,ce191e4f5bbf8a88412b793fbb1e6ff7b0ba1912@134.17.6.22:26657,801be7658f86f0c19f329d5dbf15a155d7434480@167.160.90.74:29356,94013e2b8be18361f3091287681b63f132475808@218.155.161.169:30355,2231b2285c3ba2f0dec145633d5bc90b8cf782bd@161.97.77.219:26656,843c14811951b44e7a55e7086d93f5425b549321@213.217.234.65:26656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" \
+       $HOME/.atomone/config/config.toml
+
+# Pruning Settings
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.atomone/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.atomone/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.atomone/config/app.toml
+
+# Download the snapshot
+# printGreen "12. Downloading snapshot and starting node..." && sleep 1
+
+
+
+
+
+# Start the node
+printGreen "13. Starting the node..."
+sudo systemctl start atomoned
+
+# Check node status
+printGreen "14. Checking node status..."
+sudo journalctl -u atomoned -f -o cat
+
+# Verify if the node is running
+if systemctl is-active --quiet atomoned; then
+  echo "The node is running successfully! Logs can be found at /var/log/atomone_node_install.log"
+else
+  echo "The node failed to start. Logs can be found at /var/log/atomone_node_install.log"
+fi
